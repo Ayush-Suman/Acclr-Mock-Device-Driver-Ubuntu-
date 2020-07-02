79fd592ddec19f4a9176e2681af4c0b86d7d4235
Ayush Suman <ayush231suman@gmail.com>
2020-07-02 19:38:46 +0530

    Delete .CharDeDrv.ko.cmd

--- .CharDeDrv.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/ayush231suman/LinuxKernelModule/CharDeDrv.ko := ld -r -m elf_x86_64 -z max-page-size=0x200000 -T ./scripts/module-common.lds --build-id  -o /home/ayush231suman/LinuxKernelModule/CharDeDrv.ko /home/ayush231suman/LinuxKernelModule/CharDeDrv.o /home/ayush231suman/LinuxKernelModule/CharDeDrv.mod.o ;  true
